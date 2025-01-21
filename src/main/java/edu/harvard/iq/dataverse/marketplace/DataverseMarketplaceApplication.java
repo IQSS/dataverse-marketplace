80de17e0e1101b524f3d3e2051c7419c216fba06
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-01-21 15:17:54 -0500

    Add initial project structure with Docker support and PostgreSQL configuration

---
+++ src/main/java/edu/harvard/iq/dataverse/marketplace/DataverseMarketplaceApplication.java
@@ -0,0 +1,12 @@
+package edu.harvard.iq.dataverse.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataverseMarketplaceApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(DataverseMarketplaceApplication.class, args);
+	}
+}
