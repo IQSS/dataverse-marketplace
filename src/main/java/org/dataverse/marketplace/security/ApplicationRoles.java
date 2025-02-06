41d116e3abb5e579a1a277e29e70ae45c8df1a71
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-02-06 14:42:31 -0500

    Simplify ADMIN_ROLE definition by removing RoleEnum reference

--- src/main/java/org/dataverse/marketplace/security/ApplicationRoles.java
+++ src/main/java/org/dataverse/marketplace/security/ApplicationRoles.java
@@ -1,8 +1,6 @@
 package org.dataverse.marketplace.security;
 
-import org.dataverse.marketplace.model.enums.RoleEnum;
-
 public abstract class ApplicationRoles {
 
-    public static final String ADMIN_ROLE = "hasAuthority('" + RoleEnum.ADMIN + "')";
+    public static final String ADMIN_ROLE = "hasAuthority('ADMIN')";
 }
