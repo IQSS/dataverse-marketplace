8b98925e31a03d2ef1f130734098cfbeabed0260
scolapasta <gdurand@iq.harvard.edu>
2025-05-08 16:24:56 -0400

    added EDITOR role to flyway

--- src/main/resources/db/migration/V1_0_2__user_changes.sql
+++ src/main/resources/db/migration/V1_0_2__user_changes.sql
@@ -5,3 +5,6 @@ ADD COLUMN owner_id integer NOT NULL;
 -- Add the foreign key constraint
 ALTER TABLE marketplace_item
 ADD CONSTRAINT fk_marketplace_item_owner FOREIGN KEY (owner_id) REFERENCES "users"(id);
+
+-- add new role
+insert into roles (id, role_name) values (nextval('role_id_seq'), 'EDITOR');
