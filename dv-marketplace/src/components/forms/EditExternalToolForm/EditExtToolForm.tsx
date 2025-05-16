a2fb7b77e6f36fb9d75f6aa59ac92ccf75f7447b
scolapasta <gdurand@iq.harvard.edu>
2025-05-15 22:04:32 -0400

    added a (return to) view button

--- dv-marketplace/src/components/forms/EditExternalToolForm/EditExtToolForm.tsx
+++ dv-marketplace/src/components/forms/EditExternalToolForm/EditExtToolForm.tsx
@@ -1,6 +1,7 @@
 import useEditToolForm from './useEditToolForm';
 import { FormInputTextField, FormInputTextArea } from '../../UI/FormInputFields';
-import { Alert } from 'react-bootstrap';
+import { Alert, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import EditImageForm from './EditImageForm/EditImageForm';
 import EditVersionForm from './EditVersionForm/EditVersionForm';
 
@@ -17,20 +18,26 @@ const EditExtToolForm = () => {
             <Alert variant='light'>
                 <div className='container '>
                     <div className='row'>
-                    <h1 className='col-6'>{tool?.name}:</h1>
+                        <h1 className='col-6'>{tool?.name}:</h1>
                     </div>
                 </div>
             </Alert>
             <form onSubmit={handleSubmit} encType="multipart/form-data">
                 <FormInputTextField label="Tool Name" name="name" id="toolName" value={tool?.name} />
                 <FormInputTextArea label="Description" name="description" id="toolDescription" value={tool?.description} />
-                <button type="submit" className="btn btn-primary">Update</button>
+                <Button variant="primary" type="submit">
+                    Update
+                </Button>
+                <Button variant="secondary" className="bi bi-arrow-return-left ms-2"
+                as={Link as any} to={`/view/${tool?.id}`}>
+                    <span className="me-1"></span>View
+                </Button>
             </form>
 
-            <hr/>
-            <EditVersionForm tool={tool}/>
-            
-            <hr/>
+            <hr />
+            <EditVersionForm tool={tool} />
+
+            <hr />
             <EditImageForm tool={tool} />
 
         </div>
