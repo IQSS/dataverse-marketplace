0e5b00ee0d43f1aea9958179a8c6b4624d8a6d55
Juan Pablo Tosca Villanueva <jptosca@gmail.com>
2025-05-28 12:47:31 -0400

    Update parameter schema format and enhance request body description in ExternalToolsAPIDocs

--- src/main/java/org/dataverse/marketplace/openapi/annotations/ExternalToolsAPIDocs.java
+++ src/main/java/org/dataverse/marketplace/openapi/annotations/ExternalToolsAPIDocs.java
@@ -65,7 +65,7 @@ public @interface ExternalToolsAPIDocs {
                         @ApiResponse(responseCode = "500", description = "Internal Server Error during External Tool retrieval", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ServerMessageResponse.class), examples = @ExampleObject(GenericBusinessSamples.SERVER_MESSAGE_RESPONSE)))
         })
         @Operation(summary = "Retrieves the information from the specified external tool.", description = "This endpoint retrieves the information from the specified external tool by id.")
-        @Parameter(name = "toolId", description = "The id of the external tool to be retrieved", required = true, in = ParameterIn.PATH, schema = @Schema(type = "integer", format
+        @Parameter(name = "toolId", description = "The id of the external tool to be retrieved", required = true, in = ParameterIn.PATH, schema = @Schema(type = "integer", format = "int64"))
     public @interface GetExternalToolByIdDoc {
     }
 
@@ -94,7 +94,7 @@ public @interface ExternalToolsAPIDocs {
     })
     @Operation(summary = "Adds a new image to the specified external tool.", description = "This endpoint adds a new image to the specified external tool by id.")
     @Parameter(name = "toolId", description = "the id of the external tool to add the image", required = true, in = ParameterIn.PATH, schema = @Schema(type = "integer"))        
-    @RequestBody(description = "The add tool image body")
+    @RequestBody(description = "The add tool image body request")
     public @interface AddToolImagesDoc {
     }
 
